622b6c8df90275454c392e07ada10ce368c033a4
Roopa <roopab77@yahoo.com>
2018-10-26 12:24:52 -0400

    saving my changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,4 +5,16 @@ module.exports = function(app){
   app.get("/", function(req, res) {
     res.render("index", {});
   });
+
+  app.get("/signin", function(req,res){
+    res.render("signin",{});
+  });
+
+  app.post("/signup",function(req,res){
+    res.render("signout",{});
+  });
+
+
+
+
 }
